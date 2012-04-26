6c332a264c5eb202f7085a23ddbeeccfe304c5e0
Jon Povey <jon@leetfighter.com>
2012-04-27 02:54:54 +0900

    Add tests of short literals

---
+++ tests/short-literals.s
@@ -0,0 +1,18 @@
+; test short literals
+	SET a, -300		; long
+	SET a, -3		; long
+	SET a, -2		; long
+	SET a, -1		; short in 1.5
+	SET a, 0
+	SET a, 1
+	SET a, 2
+	SET a, 3
+	SET a, 4
+	SET a, 5
+	; ...
+	SET a, 0x1d
+	SET a, 0x1c
+	SET a, 0x1e		; short
+	SET a, 0x1f		; long in 1.5
+	SET a, 0x20
+	SET a, 0x200
