865c13089e58fa0d10d12baa5fe31037df3fa72f
Jon Povey <jon@leetfighter.com>
2012-05-09 01:23:18 +0900

    Add my release script

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+if [ $# -lt 1 ]; then
+  echo "need n.n version argument"
+  exit 1
+fi
+
+VER=$1
+DIR=bluedas-v$VER
+
+make && make WINDOWS=1
+rm -rf $DIR
+mkdir -p $DIR
+cp README.md $DIR/README.md.txt
+cp das $DIR
+tar zcf $DIR.tar.gz $DIR
+rm $DIR/das
+cp das.exe $DIR
+zip -9 -r -q $DIR.zip $DIR
+echo done $DIR
