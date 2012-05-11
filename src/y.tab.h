2870cf46b4beff8d572a304c4a4d39a88fa9687c
Jon Povey <jon@leetfighter.com>
2012-05-11 23:39:29 +0900

    Whoops, forgot to add lexer and parser sources

---
+++ src/y.tab.h
@@ -0,0 +1,101 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     SYMBOL = 258,
+     LABEL = 259,
+     STRING = 260,
+     CONSTANT = 261,
+     REG = 262,
+     OP1 = 263,
+     OP2 = 264,
+     DAT = 265,
+     OPERATOR = 266,
+     LSHIFT = 267,
+     RSHIFT = 268,
+     EQU = 269,
+     UMINUS = 270
+   };
+#endif
+/* Tokens.  */
+#define SYMBOL 258
+#define LABEL 259
+#define STRING 260
+#define CONSTANT 261
+#define REG 262
+#define OP1 263
+#define OP2 264
+#define DAT 265
+#define OPERATOR 266
+#define LSHIFT 267
+#define RSHIFT 268
+#define EQU 269
+#define UMINUS 270
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1676 of yacc.c  */
+#line 22 "src/das.y"
+
+	int  integer;
+	char *string;
+	struct expr *expr;
+	struct operand *operand;
+	struct dat_elem *dat_elem;
+	struct symbol *symbol;
+
+
+
+/* Line 1676 of yacc.c  */
+#line 93 "src/y.tab.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
